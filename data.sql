2f7926e414c182032c8193f7bee9fcf09ef91932
Alexandre <chorizosurffista@gmail.com>
2021-12-05 16:21:41 +0100

    Debut base de données 6 tables créer + données sample créeramélioration a appporter

---
+++ data.sql
@@ -0,0 +1,33 @@
+USE SurfSocialMedia;
+
+INSERT INTO countries (countryName)
+VALUES("France"),("Esapgne"),("Portugal");
+
+INSERT INTO towns(countryId, townName)
+VALUES
+(1, "Vannes"), (1, "Quiberon"),(1, "Plouharnel"),
+(2, "Burela"),(2, "Viveiro"),(2, "Xove"),
+(3, "Lisboa"),(3, "Nazare"),(3, "Costa Nova");
+
+INSERT INTO usersProfils(countryId, townId, firstName, lastName, userName, birthday, email, password, homeSpot, description)
+
+VALUES (1, 1, "Alexandre", "Sage", "lalalala","14/09/1993", "alexandresage56@****.com", "password", "Port Blanc", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+
+(2, 2, "Eduardo", "Muniz", "lelelele", "14/09/1990", "eduardomuniz@****.com", "password", "Barreiros", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+
+(3, 2, "Joan", "Ramirez", "lolololo", "14/09/1995", "joandramirez@****.com", "password", "Port Blanc", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+INSERT INTO forumCategories (categoriyName)
+VALUES ("General"),("Report"),("Matos"),("DIY");
+
+
+INSERT INTO forumTopics (categoryId, userId, topicTitle, topicFirstContent)
+VALUES(1, 2, "Test topic catgory general", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+
+(2, 1, "Test topic catgory report", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+
+(3, 3, "Test topic catgory matos", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+
+(3, 2, "Test topic catgory matos", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+
+(4, 1, "Test topic catgory DIY", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
---
+++ surfSocialMedia.sql
@@ -0,0 +1,106 @@
+
+CREATE SCHEMA SurfSocialMedia;
+USE SurfSocialMedia;
+
+CREATE TABLE countries(
+    countryId INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    countryName VARCHAR(200) UNIQUE
+)ENGINE=InnoDB;
+
+
+CREATE TABLE towns(
+    townId INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+
+/*FOREIGN KEY countryId*/
+    countryId INTEGER NOT NULL,
+    KEY foreignKeyCountryId_town(countryId),
+    CONSTRAINT `foreignKeyCountryId_town`
+    FOREIGN KEY (countryId) REFERENCES countries(countryId)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+
+/*FIN FOREIGN KEYS countryId*/
+    townName VARCHAR(500) UNIQUE
+)ENGINE=InnoDB;
+
+
+CREATE TABLE usersProfils(
+    userId INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+
+/*FOREIGN KEY countryId*/
+    countryId INTEGER NOT NULL,
+    KEY foreignKeyCountryId_user(countryId),
+    CONSTRAINT `foreignKeyCountry_user`
+    FOREIGN KEY (countryId) REFERENCES countries(countryId)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+/*FIN FOREIGN KEY countryId*/
+
+/*FOREIGN KEY tonwnId*/
+    townId INTEGER NOT NULL,
+    KEY foreginKeyTownId_user(townId),
+    CONSTRAINT `foreignKeytOWN_user`
+    FOREIGN KEY (townId) REFERENCES towns(townId)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+/*FIN FOREIGN KEY townId*/
+
+    firstName VARCHAR(30) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    userName VARCHAR(50) UNIQUE NOT NULL,
+    birthday VARCHAR(10) NOT NULL,
+    email VARCHAR(500) NOT NULL UNIQUE,
+    password VARCHAR(250) NOT NULL,
+    homeSpot VARCHAR(50),
+    description VARCHAR(350),
+    creationDate DATETIME
+)ENGINE=InnoDB;
+
+/*TABLE FORUM*/
+
+CREATE TABLE forumCategories(
+    categoryId INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    categoriyName VARCHAR(1500) UNIQUE NOT NULL
+)ENGINE=InnoDB;
+
+CREATE TABLE forumTopics(
+    topicId INTEGER PRIMARY KEY AUTO_INCREMENT,
+
+/*DEBUT FOREIGN KEY cayegoryId*/
+    categoryId INTEGER NOT NULL,
+    KEY foreignKeyCategoryId_topic(categoryId),
+    CONSTRAINT `foreignKeyCategoryId_topic`
+    FOREIGN KEY (categoryId) REFERENCES forumCategories (categoryId)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+    /*FIN FOREIGN KEY categoryId*/
+
+/*DEBUT FOREIGN KEY userId*/
+    userId INTEGER NOT NULL,
+    KEY foreignKeyUserId(userID),
+    CONSTRAINT `foreignKeyUserId_topic`
+    FOREIGN KEY (userId) REFERENCES usersProfils (userId)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+/*FIN FOREIGN KEY userId*/
+
+    topicTitle VARCHAR(150) NOT NULL UNIQUE,
+    topicFirstContent VARCHAR(3500) NOT NULL
+)ENGINE=InnoDB;
+
+CREATE TABLE forumPosts(
+    postId INTEGER PRIMARY KEY AUTO_INCREMENT,
+
+/*DEBUT FOREIGN KEY topicId*/
+    topicId INTEGER NOT NULL,
+    KEY foreignKeytopicId_post(topicId),
+    CONSTRAINT `foreignKeytopicId_post`
+    FOREIGN KEY (topicId) REFERENCES forumTopics (topicId)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+/*FIN FOREIGN KEY topicId*/
+
+/*DEBUT FOREIGN KEY userId*/
+    userId INTEGER NOT NULL,
+    KEY foreignKeyUserId_topic(userID),
+    CONSTRAINT `foreignKeyUserId_post`
+    FOREIGN KEY (userId) REFERENCES usersProfils (userId)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+/*FIN FOREIGN KEY userId*/
+
+    postContent VARCHAR(3500)
+)ENGINE=InnoDB;
